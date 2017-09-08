2cb1e41463037de7758fef81f4a30225afefeca6
Silocean <silenceocean@.live.com>
2017-09-08 09:18:36 +0800

    push

---
+++ chap_3/ex3_8.c
@@ -0,0 +1,16 @@
+//
+// Created by Silocean on 2017-09-07.
+//
+
+#include <stdio.h>
+
+int main() {
+    float num;
+    scanf("%f", &num);
+    printf("%f\n", num / 2);
+    printf("%f\n", num * 8);
+    printf("%f\n", num * 8 * 2);
+    printf("%f\n", num * 8 * 2 * 3);
+    return 0;
+}
+
