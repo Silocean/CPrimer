2aaf7954e815d57b430c052e4768b51fc5714343
Silocean <silenceocean@.live.com>
2017-09-10 14:48:41 +0800

    update ex5_9.c

--- chap_5/ex5_9.c
+++ chap_5/ex5_9.c
@@ -10,10 +10,11 @@ const double c = 32.0;
 const double d = 273.16;
 
 void Temperatures(double temp) {
+    printf("Fahrenheit:%.2lf\n", temp);
     double res1 = a / b * (temp - c);
-    printf("摄氏温度：%lf\n", res1);
+    printf("centigrade:%.2lf\n", res1);
     double res2 = res1 + d;
-    printf("开氏温度：%lf\n",res2);
+    printf("kelvin:%.2lf\n",res2);
 }
 
 int main() {
